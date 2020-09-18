d660c2693efafe46bcc1b12b80a4c60ec2de7f12
masonmeier <57198981+masonmeier@users.noreply.github.com>
2020-09-17 23:02:46 -0700

    completed work on landing page revamp

--- src/components/Structure/NavBar.js
+++ src/components/Structure/NavBar.js
@@ -1,9 +1,9 @@
 import React, {useContext} from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../CSS/index.css';
 import {useHistory} from 'react-router-dom';
 import {QuizContext} from '../QuizProvider';
+import titleimage from '../pages/images/lpimages/title.png';
 
 
 const NavBar = (props) => {
@@ -19,12 +19,12 @@ const NavBar = (props) => {
     <div className='navBar'>
       <header id="nav" className="sticky-nav">
         <nav className="w-container">
-          <ul role="list" className="nav-grid w-list-unstyled">
-            <li id="w-node-ded4bd0810a5-d3ab1131"><a href="#" className="nav-logo-link w-inline-block"><img
-              src="images/7561-planet2.gif" width="80" height="80" alt="" className="nav-logo"/></a></li>
-            <li><a href="#" className="nav-link"></a></li>
+          <ul className="nav-grid w-list-unstyled">
+            <li id="w-node-ded4bd0810a5-d3ab1131"><a href="#" className="nav-logo-link w-inline-block">
+              <img src={titleimage} width="80" height="80" af-el="nav-logo" alt="" className="nav-logo"/></a>
+            </li>
             <li><a href="#" className="nav-link">Contact</a></li>
-            <li><a href="#" className="button w-button">Restart Quiz</a></li>
+            <li><a onClick={restart} className="button w-button">Restart Quiz</a></li>
           </ul>
         </nav>
       </header>
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../../CSS/landingPage.css';
 import {useHistory} from 'react-router-dom';
 import NavBar from '../Structure/NavBar';
+import titleImage from './images/lpimages/title.png';
+import planeWindow from './images/lpimages/10398-plane-window.gif';
 
 
 function LandingPage() {
@@ -14,28 +16,23 @@ function LandingPage() {
     <div className="landing-Page">
       <NavBar/>
       <div className="w-row">
-        <div className="w-col w-col-9">
-          <header id="hero" className="hero">
+        <div className="w-col-9">
+          <header id="hero" data-w-id="39ea50f5-681e-bdaf-a888-6d6d4a8942ba" className="hero">
             <div className="flex-container w-container">
-              <div>
-                <h1 className="heading">Have you ever wondered...</h1>
-                <p className="paragraph">Where in the world you would actually fit in?<br/>‍<br/>Where-To is an
-                  application that tests your preferences against dozens of international <br/>ranking metrics to
-                  determine where you<br/>would actually enjoy living!</p>
-                <button onClick={startQuiz} className="start-button w-button">Start</button>
+              <div className="lp-hero-text">
+                <h1 af-el="landing-page-header" className="heading">Have you ever wondered...</h1>
+                <p af-el="landing-page-text" className="paragraph">Where in the world you would actually fit
+                  in?<br/>‍<br/>Where-To is an application that tests your preferences against dozens of
+                  international <br/>ranking metrics to determine where you would actually enjoy living!</p>
+                <button className="start-button w-button" onClick={startQuiz}>Start</button>
               </div>
-              <div className="hero-image-mask"><img
-                src={require('./images/lpimages/title.png')}
-                alt='' className='hero-image'/>
+              <div className="hero-image-mask">
+                <img src={planeWindow}
+                     sizes="(max-width: 479px) 87vw, (max-width: 767px) 92vw, 39vw"
+                     alt="title image" className="hero-image"/>
               </div>
             </div>
           </header>
-        </div>
-        <div className="column w-col w-col-3">
-          <img src={require('./images/lpimages/10398-plane-window.gif')}
-               loading="lazy"
-               alt=""
-               className="image"/>
         </div>
       </div>
       <div className="signature-holder">
