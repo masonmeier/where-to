e3c68d7b330962209a3fb82451ae12ba42421e35
masonmeier <57198981+masonmeier@users.noreply.github.com>
2020-10-02 16:20:50 -0700

    added a .env file for SSL certification

--- src/components/QuizProvider.js
+++ src/components/QuizProvider.js
@@ -68,5 +68,5 @@ class QuizProvider extends Component {
 
 export default QuizProvider;
 export const QuizContext = React.createContext();
-export const remoteServerURL = 'http://ec2-54-190-70-0.us-west-2.compute.amazonaws.com:3002';
-// export const remoteServerURL = 'http://localhost:3002';
+// export const remoteServerURL = 'http://ec2-54-190-70-0.us-west-2.compute.amazonaws.com:3002';
+export const remoteServerURL = 'http://localhost:3002';
